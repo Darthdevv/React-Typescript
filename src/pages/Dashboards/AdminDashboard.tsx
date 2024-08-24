427b7a06aa43abc01e74551bf3c77ed09f48f779
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2024-08-25 02:07:16 +0300

    ⚛️ fetching data into table

--- src/pages/Dashboards/AdminDashboard.tsx
+++ src/pages/Dashboards/AdminDashboard.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import {
   Table,
   TableBody,
@@ -7,27 +8,79 @@ import {
   TableHeader,
   TableRow,
 } from "../../components/ui/table";
+import axios from "axios";
+
+interface Geo {
+  lat: string;
+  lng: string;
+}
+
+interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+}
+
+interface Company {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
+
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: Address;
+  phone: string;
+  website: string;
+  company: Company;
+}
 
 const AdminDashboard = () => {
+  const [users, setUsers] = useState<User[]>([]);
+  const BASE_URL: string = "https://jsonplaceholder.typicode.com/";
+
+  const GetUsers = async () => {
+    const {data} = await axios.get(`${BASE_URL}/users`);
+    setUsers(data);
+    console.log(data);
+  }
+
+  useEffect(() => {
+    GetUsers();
+  },[])
+
   return (
     <Table>
-      <TableCaption>A list of your recent invoices.</TableCaption>
+      <TableCaption>A list of your users.</TableCaption>
       <TableHeader>
         <TableRow>
-          <TableHead className="w-[100px]">Invoice</TableHead>
-          <TableHead>Status</TableHead>
-          <TableHead>Method</TableHead>
-          <TableHead className="text-right">Amount</TableHead>
+          <TableHead className="w-[100px]">Id</TableHead>
+          <TableHead>Name</TableHead>
+          <TableHead>Username</TableHead>
+          <TableHead className="text-right">Email</TableHead>
+          <TableHead className="text-right">Phone</TableHead>
+          <TableHead className="text-right">Website</TableHead>
         </TableRow>
       </TableHeader>
-      <TableBody>
-        <TableRow>
-          <TableCell className="font-medium">INV001</TableCell>
-          <TableCell>Paid</TableCell>
-          <TableCell>Credit Card</TableCell>
-          <TableCell className="text-right">$250.00</TableCell>
-        </TableRow>
-      </TableBody>
+      {users.map((user) => {
+        return (
+          <TableBody>
+            <TableRow>
+              <TableCell className="font-medium">{user.id}</TableCell>
+              <TableCell>{user.name}</TableCell>
+              <TableCell>{user.username}</TableCell>
+              <TableCell className="text-right">{user.email}</TableCell>
+              <TableCell>{user.phone}</TableCell>
+              <TableCell>{user.website}</TableCell>
+            </TableRow>
+          </TableBody>
+        );
+      })}
     </Table>
   );
 };
