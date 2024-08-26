f1bdf20911efcbf9e0ede2604e086b516103d153
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2024-08-27 00:43:30 +0300

    ⚛️ drag and drop file upload component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import './App.css'
 // import Albums from './pages/Albums/Albums'
 // import Login from './pages/Log/Login'
-import AdminDashboard from './pages/Dashboards/AdminDashboard'
+// import AdminDashboard from './pages/Dashboards/AdminDashboard'
+import { FileDrop } from './components/FileDrop'
 
 function App() {
 
   return (
     <div>
-      <AdminDashboard />
+      <FileDrop/>
     </div>
   )
 }
